c8e99832f4378b16739c0e0fafa8a9846585fe8d
GHJGJ-PC\ABC <AppStar1108@outlook.com>
2017-02-12 23:06:29 +0800

    Artistry Starter source for iPhone App

---
+++ Artistry/ArtistTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  ArtistTableViewCell.swift
+//  Artistry
+//
+//  Created by Prince on 1/26/17.
+//  Copyright © 2017 RayWenderlich. All rights reserved.
+//
+
+import UIKit
+
+class ArtistTableViewCell: UITableViewCell {
+
+//    override func awakeFromNib() {
+//        super.awakeFromNib()
+//        // Initialization code
+//    }
+//
+//    override func setSelected(_ selected: Bool, animated: Bool) {
+//        super.setSelected(selected, animated: animated)
+//
+//        // Configure the view for the selected state
+//    }
+  @IBOutlet weak var bioLabel: UILabel!
+  @IBOutlet weak var nameLabel: UILabel!
+  @IBOutlet weak var artistImageView: UIImageView!
+
+
+}
---
+++ Artistry/WorkTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  WorkTableViewCell.swift
+//  Artistry
+//
+//  Created by Prince on 1/26/17.
+//  Copyright © 2017 RayWenderlich. All rights reserved.
+//
+
+import UIKit
+
+class WorkTableViewCell: UITableViewCell {
+
+  @IBOutlet weak var workImageView: UIImageView!
+  @IBOutlet weak var workTitleLabel: UILabel!
+  @IBOutlet weak var moreInfoTextView: UITextView!
+
+}
